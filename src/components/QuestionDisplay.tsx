e99b2400dbed0d2eb81c1dd33f6198a808668a39
kloir-z <kosuke.otsu@gmail.com>
2024-11-28 02:00:21 +0900

    一旦完成版

--- src/components/QuestionDisplay.tsx
+++ src/components/QuestionDisplay.tsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import { Question } from '../types';
 
 export const QuestionDisplay = ({ question }: { question: Question }) => {
+    const [isAnswerShown, setIsAnswerShown] = useState(false);
     const parts = question.text.split(question.kanji);
     const kanjiOnly = /^[\u4E00-\u9FFF]+$/;
     // eslint-disable-next-line no-irregular-whitespace
     const readingText = `　${question.reading}`;
     const isKanjiOnly = kanjiOnly.test(question.kanji);
 
+    const toggleAnswer = () => {
+        setIsAnswerShown(!isAnswerShown);
+    };
+
     return (
-        <div className="min-w-[120px] mx-4 flex justify-center">
+        <div
+            className="min-w-[120px] mx-4 flex justify-center cursor-pointer font-mincho"
+            onClick={toggleAnswer}
+        >
             <div className="writing-vertical inline-flex flex-col items-center text-2xl whitespace-nowrap">
                 <div className="flex items-center">
                     {parts.map((part, partIndex, array) => (
@@ -19,17 +28,24 @@ export const QuestionDisplay = ({ question }: { question: Question }) => {
                                     <span className="inline-block relative">
                                         {isKanjiOnly ? (
                                             <span className="text-4xl mx-auto flex">
-                                                {Array.from(question.kanji).map((_, idx) => (
-                                                    <span key={idx} className="kanji-box"></span>
+                                                {Array.from(question.kanji).map((char, idx) => (
+                                                    <span key={idx} className="kanji-box flex items-center justify-center">
+                                                        {isAnswerShown && (
+                                                            <span className="text-red-600">{char}</span>
+                                                        )}
+                                                    </span>
                                                 ))}
                                             </span>
                                         ) : (
-                                            <span className="text-5xl mx-auto flex border border-gray-600 px-4 py-9 min-h-[144px] flex items-center mt-2">
-                                                {/* 漢字の文字数分の全角スペースを表示 */}
-                                                {Array.from(question.kanji).map((_, idx) => (
-                                                    // eslint-disable-next-line no-irregular-whitespace
-                                                    <span key={idx} className="writing-vertical">　</span>
-                                                ))}
+                                            <span className="text-4xl mx-auto flex border border-gray-600 px-5 py-9 min-h-[144px] flex items-center mt-2">
+                                                {isAnswerShown ? (
+                                                    <span className="text-red-600 writing-vertical tracking-[0.22em]">{question.kanji}</span>
+                                                ) : (
+                                                    Array.from(question.kanji).map((_, idx) => (
+                                                        // eslint-disable-next-line no-irregular-whitespace
+                                                        <span key={idx} className="writing-vertical mb-2">　</span>
+                                                    ))
+                                                )}
                                             </span>
                                         )}
                                         <span className="absolute top-0 -right-7 text-sm ruby-text">
